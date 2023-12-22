0f4edcfde1b21ce1d3a885645f45623b18e4d58a
Egle Laboureur <egle.laboureur@gmail.com>
2023-12-22 16:30:16 +0100

    created buttons

--- cssReactCarte/src/pages/Home.jsx
+++ cssReactCarte/src/pages/Home.jsx
@@ -1,6 +1,23 @@
-function Home () {
-    return <h1>Surprise</h1>;
 
-}
+function Home () {
+    const handleClick = () => {
+        //console.log()
+    };
+  
+    return (
+    <>
+     <h1>Surprise</h1>
+     <button onClick={handleClick}>
+    Get your card!    
+    </button>
+    
+    <button onClick={handleClick}> 
+    Get a card for your mom    
+    </button>  
+   </>
+    );
+
+};
+
 
 export default Home; 
